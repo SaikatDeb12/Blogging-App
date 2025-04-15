79b6abab46f64be90fa4740a026f4928f9083dbb
SaikatDeb12 <saikat.rcld@gmail.com>
2025-04-15 13:59:18 +0530

    uploading form with image

--- routes/blog.js
+++ routes/blog.js
@@ -1,5 +1,20 @@
 const { Router } = require("express");
 const router = Router();
+const multer = require("multer");
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(
+      null,
+      `C:/Users/saika/OneDrive/Documents/studies_related/node_basics/blog-App-JWT/public/uploads/`
+    );
+  },
+  filename: function (req, file, cb) {
+    const fileName = `${Date.now()}-${file.originalname}`;
+    cb(null, fileName);
+  },
+});
+
+const upload = multer({ storage: storage });
 
 router.get("/add-blog", (req, res) => {
   return res.render("addBlog", {
@@ -7,8 +22,9 @@ router.get("/add-blog", (req, res) => {
   });
 });
 
-router.post("/", (req, res) => {
+router.post("/", upload.single("coverImage"), (req, res) => {
   console.log(req.body);
+  console.log(req.file);
   return res.redirect("/");
 });
 
