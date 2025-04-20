cd2e5d6f428eb408263ff4cb6dbf6e853035d8f4
SaikatDeb12 <saikat.rcld@gmail.com>
2025-04-20 10:58:39 +0530

    adding comment model

---
+++ model/comment.js
@@ -0,0 +1,22 @@
+const { model, Schema } = require("mongoose");
+
+const commentSchema = new Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    blogId: {
+      type: Schema.Types.ObjectId,
+      ref: "blog",
+    },
+    createdBy: {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+    },
+  },
+  { timestamps: true }
+);
+
+const commentModel = model("comment", commentSchema);
+module.exports = commentModel;
